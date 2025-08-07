47adcc19ef3c40f5f27e1a248f1ea4fbcaba679a
Moises David Maldonado de León <moiseszurdomaldonado@gmail.com>
2025-08-07 13:04:32 -0600

    feat(ml): KNN

--- src/supervisado/4.1K-Nearest Neighbors.py
+++ src/supervisado/4.1K-Nearest Neighbors.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import joblib
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
@@ -31,32 +32,36 @@ X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
 # --- 5. Definir y entrenar el modelo ---
-knn = KNeighborsClassifier(n_neighbors=3)
+knn = KNeighborsClassifier(n_neighbors=7, weights='distance')
 knn.fit(X_train_scaled, y_train)
 
 # --- 6. Predicción y métricas ---
 y_pred = knn.predict(X_test_scaled)
 accuracy = accuracy_score(y_test, y_pred)
 
-print("\n📊 Métricas de evaluación:")
+print("\nMétricas de evaluación:")
 print("Accuracy:", accuracy)
 print("\nClassification Report:")
 print(classification_report(y_test, y_pred))
 print("\nMatriz de Confusión:")
 print(confusion_matrix(y_test, y_pred))
 
-# --- 7. Visualización simple con dos features ---
-# Solo para visualización: usamos dos features representativas
-X_vis = X_train_scaled[:, :2]  # Usamos solo las 2 primeras columnas
-y_vis = y_train.to_numpy()
+# # --- 7. Visualización simple con dos features ---
+# # Solo para visualización: usamos dos features representativas
+# X_vis = X_train_scaled[:, :2]  # Usamos solo las 2 primeras columnas
+# y_vis = y_train.to_numpy()
 
-plt.figure(figsize=(8, 6))
-colores = ['red', 'green', 'blue']
-for i in range(len(X_vis)):
-    plt.scatter(X_vis[i, 0], X_vis[i, 1], color=colores[y_vis[i]], s=50, alpha=0.6)
+# plt.figure(figsize=(8, 6))
+# colores = ['red', 'green', 'blue']
+# for i in range(len(X_vis)):
+#     plt.scatter(X_vis[i, 0], X_vis[i, 1], color=colores[y_vis[i]], s=50, alpha=0.6)
 
-plt.title("Visualización KNN (con 2 primeras features)")
-plt.xlabel(features[0])
-plt.ylabel(features[1])
-plt.grid(True)
-plt.show()
+# plt.title("Visualización KNN (con 2 primeras features)")
+# plt.xlabel(features[0])
+# plt.ylabel(features[1])
+# plt.grid(True)
+# plt.show()
+
+# guardar el modelo completo
+joblib.dump(knn, "models/KNN/KNN_classifier.joblib")
+joblib.dump(scaler, "models/KNN/scaler_knn.joblib")
